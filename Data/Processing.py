7e3be1c3f52fee47d8a91db890f202b6b62ed611
CastyLny <49752325+CastyLny@users.noreply.github.com>
2021-10-03 00:10:09 -0400

    Add new features and deleted the graph

--- Data/Processing.py
+++ Data/Processing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 co2 = pd.read_csv("annual-co2-emissions-per-country.csv")
 ocean = pd.read_csv("WaterLevel.csv")
 years = np.unique(co2['Year'].to_numpy())
@@ -9,24 +10,33 @@ matrixYear = np.zeros((years.size, 3))
 for i, year in enumerate(years):
     matrixYear[i] = [year, np.sum(co2.loc[co2['Year'] == year].iloc[:,-1]), 0]
 print(ocean.iloc[:, 2].to_numpy().astype("int32"))
-
+print(ocean)
 #print(matrixYear)
 usableIndex = []
 for i, year in enumerate(ocean.iloc[:, 2].to_numpy().astype("int32")):
     try:
         usableIndex.append(np.where(matrixYear[:,0] == year)[0][0])
         matrixYear[np.where(matrixYear[:,0] == year)[0][0], -1] += ocean.iloc[i, -1]
+        print(year, ocean.iloc[i, -1])
     except:
         pass
 
 usableIndexes = np.unique(np.array(usableIndex))
 matrixYea = matrixYear[usableIndexes, :]
+m = np.polyfit(matrixYea[:,1]/1e9, matrixYea[:,2],2)
+print(m)
+def applyFunction(input, theta):
+    print(input)
+    return theta[0] * input ** 2 + theta[1] * input + theta[2]
 
 
-plt.subplot(1,2,1)
-plt.plot(matrixYea[:,0], matrixYea[:,1])
-plt.title("Global CO2 emission")
-plt.subplot(1,2,2)
-plt.title("Changes in sea level")
-plt.plot(matrixYea[:,0], matrixYea[:,2])
+
+print(np.max(matrixYea, axis = 0))
+plt.subplot(1,1,1)
+plt.plot(matrixYea[:,1]/1e9,matrixYea[:,2], label = "Actual")
+plt.plot(matrixYea[:,1]/1e9,applyFunction(matrixYea[:,1]/1e9, m), label = "Estimate")
+plt.ylabel("Rise in sea level in mm")
+plt.xlabel("CO2 Emission Globally in Giga Tons")
+plt.legend()
+#plt.plot(matrixYea[:,0], matrixYea[:,2])
 plt.show()
--- MeltingEarth/main.js
+++ MeltingEarth/main.js
@@ -3,23 +3,30 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
 
 let scene, camera, renderer, sphere;
-let x = 1;
-let dx = 1
+let radius = 1;
+let dt = 1/600;//dt is how many year passed in one frame
+let CO2Level = document.getElementById("CO2Slider")
+
+console.log(CO2Level)
+
+
+//Function that take in CO2 in gigatons to estimate sealevel in mm
+//28.85 is the calculated ratio between mount everest in the simulated model/ mount everest to earth radius
+function CO2ToSeaLevelEstimate(CO2){
+	return  28.85 * (9.22434399e-01 * Math.pow(CO2, 2) - 1.37511697e+02 * CO2 + 4.15798071e+03)/1000000 //Conversion to km
+}
 
 
 let dxSlider = document.getElementById("CLincrease")
 console.log(dxSlider)
 
-function setx(value){
-	x = 1
-}
-
-
-
 let resetButton = document.getElementById('ResetButton');
 resetButton.onclick = () => {
-	setx(1)
+	radius = 1
 }
+
+
+
 
 
 
@@ -39,15 +46,25 @@ function init(){
 	renderer = new THREE.WebGLRenderer({antialias: true});
 	renderer.setSize(window.innerWidth - 30, window.innerHeight - 30);
 	let earthDisplay = document.getElementById("Earth");
-	earthDisplay.appendChild(renderer.domElement);
+	let element = renderer.domElement
+	element.className = "EarthScene"
+	earthDisplay.appendChild(element);
 
 	let controls = new OrbitControls(camera, renderer.domElement);
 	controls.update();
 
 	function animate() {
-		x += (0.00001 * dxSlider.value)
-		sphere.scale.set( x, x,x)
-		console.log()
+		let seaLevelRise = CO2ToSeaLevelEstimate(CO2Level.value) ;
+		//console.log(seaLevelRise)
+		document.getElementById("CO2Log").innerHTML = CO2Level.value + " Giga Tones of CO2"
+		document.getElementById("CLincrease").value = seaLevelRise * 100;
+		console.log((seaLevelRise/6,378) * dt)
+		radius += seaLevelRise/6,378 * dt;
+
+		radius = Math.min(Math.max(1, radius),1.2)
+	
+		sphere.scale.set( radius, radius,radius)
+	
 		requestAnimationFrame(animate);
 		controls.update();
 		renderer.render(scene, camera)
@@ -82,7 +99,7 @@ function init(){
 		earth.scale.set(-0.4,-0.4,-0.4);
 		earth.frustumCulled = false;
 		//earth.material = new THREE.MeshStandardMaterial({color: 0x3ec43b});
-		let geometry = new THREE.SphereGeometry(7.70 * x,40,40);
+		let geometry = new THREE.SphereGeometry(7.70 * radius,40,40);
 		let material = new THREE.MeshStandardMaterial({color: 0x0022ff});
 		sphere = new THREE.Mesh(geometry, material);
 		
